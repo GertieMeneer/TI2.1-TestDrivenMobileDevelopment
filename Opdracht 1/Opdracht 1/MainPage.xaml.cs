037d7ff18d06342a4e2a357e42173bd0b0f73085
Stef Kraaijeveld <itiredplease@gmail.com>
2023-11-11 14:17:09 +0100

    add to read API

--- Opdracht 1/Opdracht 1/MainPage.xaml.cs
+++ Opdracht 1/Opdracht 1/MainPage.xaml.cs
@@ -1,34 +1,52 @@
 ﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
 namespace Opdracht_1
 {
     public partial class MainPage : ContentPage
     {
         private List<Double> data = new List<Double>();
         private HttpClient httpClient = new HttpClient();
-        private string url = "";
+        private string url = "https://bloodservice.azurewebsites.net/bloodsugar";
 
         public MainPage()
         {
-            data.Add(0);
-            data.Add(1);
-            data.Add(2);
-            data.Add(3);
-            data.Add(4);
-
             InitializeComponent();
             GetValues();
         }
 
         public async void GetValues()
         {
-            ValueList.ItemsSource = data;
-
             HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, url);
 
-            HttpResponseMessage respones = await httpClient.SendAsync(message);
+            HttpResponseMessage response = await httpClient.SendAsync(message);
 
+            if (response.IsSuccessStatusCode)
+            {
+                // Assuming the response body is a JSON array of objects.
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // Use JArray from Newtonsoft.Json to parse the JSON array.
+                JArray responseData = JArray.Parse(responseBody);
+
+                // Extract values from each object in the array.
+                foreach (JObject item in responseData)
+                {
+                    double value = (double)item["value"];
+                    // Assuming "value" is the property you want to add to the data list.
+                    data.Add(value);
+                }
+
+                // Assuming ValueList is a UI control, update its ItemsSource.
+                ValueList.ItemsSource = data;
+            }
+            else
+            {
+                // Handle the case where the request was not successful.
+                Trace.WriteLine($"Request failed with status code: {response.StatusCode}");
+            }
         }
-
     }
-
 }
