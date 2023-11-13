03fee8c527d7c13eab12b6402d327b80823f14c2
GertieMeneer <gertjan.vat@icloud.com>
2023-11-13 13:16:30 +0100

    Added more properties to lampinfopage

--- Opdracht 2/TDMD/LampInfoPage.xaml.cs
+++ Opdracht 2/TDMD/LampInfoPage.xaml.cs
@@ -6,6 +6,19 @@ public partial class LampInfoPage : ContentPage
 	{
 		InitializeComponent();
 		LampNameLabel.Text = lamp.name;
-		LampIDLabel.Text = lamp.id;
+		if (lamp.status)
+		{
+			LampStatusLabel.Text = "Lamp status: ON";
+        }
+		else
+		{
+            LampStatusLabel.Text = "Lamp status: OFF";
+        }
+
+        LampIDLabel.Text = $"Lamp ID: {lamp.id}";
+		LampTypeLabel.Text = $"Lamp type: {lamp.type}";
+		LampModelIDLabel.Text = $"Lamp Model ID: {lamp.modelid}";
+		LampSoftwareLabel.Text = $"Lamp Software: {lamp.swversion}";
+		LampUniqueIDLabel.Text = $"Lamp Unique ID: {lamp.uniqueid}";
 	}
 }
