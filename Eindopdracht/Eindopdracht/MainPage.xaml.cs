81ded64a7eb66b1e01bfadf0088dccedd6bc11ee
Stef Kraaijeveld <itiredplease@gmail.com>
2023-11-21 09:24:13 +0100

    get user location

--- Eindopdracht/Eindopdracht/MainPage.xaml.cs
+++ Eindopdracht/Eindopdracht/MainPage.xaml.cs
@@ -11,7 +11,16 @@ namespace Eindopdracht
             var location = new Location(36, -122);
             var mapSpan = new MapSpan(location, 0.01, 0.01);
 
+            GetCurrentLocation();
+
             map.MoveToRegion(mapSpan);
+        }
+        
+        public async Task GetCurrentLocation()
+        {   
+            GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+            Location location = await Geolocation.Default.GetLocationAsync(request);
         }
     }
 
